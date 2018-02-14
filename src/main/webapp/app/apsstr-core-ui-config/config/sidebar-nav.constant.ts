980905aebf8d21e2d573d5d1ad05d7d5e91b4b78
Arif Rabbani <arif.93rabbani@gmail.com>
2018-02-15 00:12:17 +0530

    Sidebar Menu Items added

--- src/main/webapp/app/apsstr-core-ui-config/config/sidebar-nav.constant.ts
+++ src/main/webapp/app/apsstr-core-ui-config/config/sidebar-nav.constant.ts
@@ -1,10 +1,45 @@
-export const SIDEBAR_NAV_ITEMS = [{
-    name: 'Dashboard',
-    url: '/dashboard',
-    icon: 'icon-speedometer',
-    badge: {
-      variant: 'info',
-      text: 'NEW'
-    }
+export const SIDEBAR_NAV_ITEMS = [
+  {
+    title: true,
+    name: 'Categories'
+  },
+  {
+    name: 'Reecharge',
+    url: '/',
+    icon: 'icon-screen-smartphone',
+    children: [
+      {
+        name: 'Mobile',
+        url: '/',
+        icon: 'icon-screen-smartphone'
+      },
+      {
+        name: 'DTH',
+        url: '/',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Datacard',
+        url: '/',
+        icon: 'icon-puzzle'
+      }
+    ]
+  },
+  {
+    name: 'Travel',
+    url: '/',
+    icon: 'icon-cursor',
+    children: [
+      {
+        name: 'Flight',
+        url: '/',
+        icon: 'icon-cursor'
+      },
+      {
+        name: 'Hotel',
+        url: '/',
+        icon: 'icon-cursor'
+      }
+    ]
   }
 ];
