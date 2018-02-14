feaeb6c9364c83a394bdcda32b32a12e7bf13874
Arif Rabbani <arif.93rabbani@gmail.com>
2018-02-14 23:01:14 +0530

    Directives Added

--- src/main/webapp/app/apsstr-core-ui/core-ui-layout/core-ui-layout.module.ts
+++ src/main/webapp/app/apsstr-core-ui/core-ui-layout/core-ui-layout.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FullLayoutComponent } from './container';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components';
+import { AsideToggleDirective, NAV_DROPDOWN_DIRECTIVES, ReplaceDirective, SIDEBAR_TOGGLE_DIRECTIVES } from './directives';
+import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
 
 const CORE_UI_CONTAINERS = [
   FullLayoutComponent
@@ -12,14 +14,24 @@ const CORE_UI_LAYOUT_COMPONENTS = [
   HeaderComponent
 ];
 
+const CORE_UI_DIRECTIVES = [
+  AsideToggleDirective,
+  NAV_DROPDOWN_DIRECTIVES,
+  ReplaceDirective,
+  SIDEBAR_TOGGLE_DIRECTIVES
+];
+
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    BsDropdownModule.forRoot(),
+    TabsModule.forRoot()
   ],
   declarations: [
     ...CORE_UI_CONTAINERS,
-    ...CORE_UI_LAYOUT_COMPONENTS
+    ...CORE_UI_LAYOUT_COMPONENTS,
+    ...CORE_UI_DIRECTIVES
   ],
   exports: [
     ...CORE_UI_CONTAINERS
