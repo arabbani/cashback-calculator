939d5c02f13dd9eb1e609ca2692476e91090363a
Arif Rabbani <arif.93rabbani@gmail.com>
2018-02-14 21:42:02 +0530

    FullLayout in place

--- src/main/webapp/app/apsstr-core-ui/core-ui-layout/core-ui-layout.module.ts
+++ src/main/webapp/app/apsstr-core-ui/core-ui-layout/core-ui-layout.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FullLayoutComponent } from './container';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './components';
 
 const CORE_UI_CONTAINERS = [
   FullLayoutComponent
 ];
 
+const CORE_UI_LAYOUT_COMPONENTS = [
+  HeaderComponent
+];
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [
-    ...CORE_UI_CONTAINERS
+    ...CORE_UI_CONTAINERS,
+    ...CORE_UI_LAYOUT_COMPONENTS
   ],
   exports: [
     ...CORE_UI_CONTAINERS
