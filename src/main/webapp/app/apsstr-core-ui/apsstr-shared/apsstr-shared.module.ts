3e32726890115fd5abd8d05a108ebb30a5e8cd82
Arif Rabbani <arif.93rabbani@gmail.com>
2018-02-16 16:38:29 +0530

    Alert Component added

--- src/main/webapp/app/apsstr-core-ui/apsstr-shared/apsstr-shared.module.ts
+++ src/main/webapp/app/apsstr-core-ui/apsstr-shared/apsstr-shared.module.ts
@@ -1,16 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { AlertModule } from 'ngx-bootstrap';
+
 import {
-    CardBodyComponent, CardComponent, CardFooterComponent, CardHeaderComponent
+    AlertComponent, CardBodyComponent, CardComponent, CardFooterComponent, CardHeaderComponent
 } from './components';
 
 const COMPONENTS = [
-  CardComponent, CardBodyComponent, CardHeaderComponent, CardFooterComponent
+  CardComponent, CardBodyComponent, CardHeaderComponent, CardFooterComponent, AlertComponent
 ];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AlertModule.forRoot()
   ],
   declarations: COMPONENTS,
   exports: COMPONENTS
