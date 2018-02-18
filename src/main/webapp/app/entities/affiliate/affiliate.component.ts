27a74de52b1d153a9c1c6685f67f22bc21b43f77
Arif Rabbani <arif.93rabbani@gmail.com>
2018-02-18 11:46:03 +0530

    In progress

--- src/main/webapp/app/entities/affiliate/affiliate.component.ts
+++ src/main/webapp/app/entities/affiliate/affiliate.component.ts
@@ -1,30 +1,30 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
-import { Subscription } from 'rxjs/Subscription';
-import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { DataStateChangeEvent } from '@progress/kendo-angular-grid';
+import { State } from '@progress/kendo-data-query';
+import { JhiAlertService } from 'ng-jhipster';
 
 import { Affiliate } from './affiliate.model';
 import { AffiliateService } from './affiliate.service';
-import { Principal } from '../../shared';
 
 @Component({
     selector: 'apsstr-affiliate',
     templateUrl: './affiliate.component.html'
 })
-export class AffiliateComponent implements OnInit, OnDestroy {
-affiliates: Affiliate[];
-    currentAccount: any;
-    eventSubscriber: Subscription;
+export class AffiliateComponent implements OnInit {
+    affiliates: Affiliate[];
+    public state: State = {
+        skip: 0,
+        take: 5
+    };
 
-    constructor(
-        private affiliateService: AffiliateService,
-        private jhiAlertService: JhiAlertService,
-        private eventManager: JhiEventManager,
-        private principal: Principal
-    ) {
+    constructor(private affiliateService: AffiliateService, private jhiAlertService: JhiAlertService) {}
+
+    ngOnInit() {
+        this.loadAll();
     }
 
-    loadAll() {
+    private loadAll() {
         this.affiliateService.query().subscribe(
             (res: HttpResponse<Affiliate[]>) => {
                 this.affiliates = res.body;
@@ -32,26 +32,56 @@ affiliates: Affiliate[];
             (res: HttpErrorResponse) => this.onError(res.message)
         );
     }
-    ngOnInit() {
+
+    public dataStateChange(state: DataStateChangeEvent): void {
+        this.state = state;
         this.loadAll();
-        this.principal.identity().then((account) => {
-            this.currentAccount = account;
-        });
-        this.registerChangeInAffiliates();
-    }
-
-    ngOnDestroy() {
-        this.eventManager.destroy(this.eventSubscriber);
-    }
-
-    trackId(index: number, item: Affiliate) {
-        return item.id;
-    }
-    registerChangeInAffiliates() {
-        this.eventSubscriber = this.eventManager.subscribe('affiliateListModification', (response) => this.loadAll());
     }
 
     private onError(error) {
         this.jhiAlertService.error(error.message, null, null);
     }
+
+// affiliates: Affiliate[];
+//     currentAccount: any;
+//     eventSubscriber: Subscription;
+
+//     constructor(
+//         private affiliateService: AffiliateService,
+//         private jhiAlertService: JhiAlertService,
+//         private eventManager: JhiEventManager,
+//         private principal: Principal
+//     ) {
+//     }
+
+//     loadAll() {
+//         this.affiliateService.query().subscribe(
+//             (res: HttpResponse<Affiliate[]>) => {
+//                 this.affiliates = res.body;
+//             },
+//             (res: HttpErrorResponse) => this.onError(res.message)
+//         );
+//     }
+//     ngOnInit() {
+//         this.loadAll();
+//         this.principal.identity().then((account) => {
+//             this.currentAccount = account;
+//         });
+//         this.registerChangeInAffiliates();
+//     }
+
+//     ngOnDestroy() {
+//         this.eventManager.destroy(this.eventSubscriber);
+//     }
+
+//     trackId(index: number, item: Affiliate) {
+//         return item.id;
+//     }
+//     registerChangeInAffiliates() {
+//         this.eventSubscriber = this.eventManager.subscribe('affiliateListModification', (response) => this.loadAll());
+//     }
+
+//     private onError(error) {
+//         this.jhiAlertService.error(error.message, null, null);
+//     }
 }
