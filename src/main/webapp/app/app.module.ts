442cc2af50f333997d6bb8dcae1d545395fdf293
Arif Rabbani <arif.93rabbani@gmail.com>
2018-02-18 11:25:15 +0530

    kendo grid installed

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -1,32 +1,28 @@
 import './vendor.ts';
 
-import { NgModule, Injector } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { Ng2Webstorage } from 'ngx-webstorage';
+import { Injector, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GridModule } from '@progress/kendo-angular-grid';
 import { JhiEventManager } from 'ng-jhipster';
+import { Ng2Webstorage } from 'ngx-webstorage';
 
+import { CbclAccountModule } from './account/account.module';
+import { CbclAdminModule } from './admin/admin.module';
+import { CbclAppRoutingModule } from './app-routing.module';
+import { ApsstrCoreModule, CoreUiLayoutModule } from './apsstr-core-ui';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
 import { AuthExpiredInterceptor } from './blocks/interceptor/auth-expired.interceptor';
 import { ErrorHandlerInterceptor } from './blocks/interceptor/errorhandler.interceptor';
 import { NotificationInterceptor } from './blocks/interceptor/notification.interceptor';
-import { CbclSharedModule, UserRouteAccessService } from './shared';
-import { CbclAppRoutingModule} from './app-routing.module';
-import { CbclHomeModule } from './home/home.module';
-import { CbclAdminModule } from './admin/admin.module';
-import { CbclAccountModule } from './account/account.module';
 import { CbclEntityModule } from './entities/entity.module';
-import { PaginationConfig } from './blocks/config/uib-pagination.config';
+import { CbclHomeModule } from './home/home.module';
+import { ApsstrMainComponent, ErrorComponent, NavbarComponent, PageRibbonComponent, ProfileService } from './layouts';
+import { CbclSharedModule, UserRouteAccessService } from './shared';
 import { StateStorageService } from './shared/auth/state-storage.service';
-// jhipster-needle-angular-add-module-import JHipster will add new module here
-import {
-    ApsstrMainComponent,
-    NavbarComponent,
-    ProfileService,
-    PageRibbonComponent,
-    ErrorComponent
-} from './layouts';
-import { CoreUiLayoutModule, ApsstrCoreModule } from './apsstr-core-ui';
 
+// jhipster-needle-angular-add-module-import JHipster will add new module here
 @NgModule({
     imports: [
         BrowserModule,
@@ -38,7 +34,9 @@ import { CoreUiLayoutModule, ApsstrCoreModule } from './apsstr-core-ui';
         CbclAccountModule,
         CbclEntityModule,
         CoreUiLayoutModule,
-        ApsstrCoreModule
+        ApsstrCoreModule,
+        BrowserAnimationsModule,
+        GridModule
         // jhipster-needle-angular-add-module JHipster will add new module here
     ],
     declarations: [
