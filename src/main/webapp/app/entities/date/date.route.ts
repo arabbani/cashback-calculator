eb4c9edc502de0ab486e18a29fe9279216b4ec69
Arif Rabbani <arif.93rabbani@gmail.com>
2018-02-26 01:21:17 +0530

    Grid integrated to date

--- src/main/webapp/app/entities/date/date.route.ts
+++ src/main/webapp/app/entities/date/date.route.ts
@@ -1,60 +1,14 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
 
 import { UserRouteAccessService } from '../../shared';
 import { DateComponent } from './date.component';
-import { DateDetailComponent } from './date-detail.component';
-import { DatePopupComponent } from './date-dialog.component';
-import { DateDeletePopupComponent } from './date-delete-dialog.component';
 
-export const dateRoute: Routes = [
-    {
-        path: 'date',
-        component: DateComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Dates'
-        },
-        canActivate: [UserRouteAccessService]
-    }, {
-        path: 'date/:id',
-        component: DateDetailComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Dates'
-        },
-        canActivate: [UserRouteAccessService]
-    }
-];
-
-export const datePopupRoute: Routes = [
-    {
-        path: 'date-new',
-        component: DatePopupComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Dates'
-        },
-        canActivate: [UserRouteAccessService],
-        outlet: 'popup'
+export const dateRoute: Route = {
+    path: 'date',
+    component: DateComponent,
+    data: {
+        authorities: ['ROLE_ADMIN'],
+        pageTitle: 'Dates'
     },
-    {
-        path: 'date/:id/edit',
-        component: DatePopupComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Dates'
-        },
-        canActivate: [UserRouteAccessService],
-        outlet: 'popup'
-    },
-    {
-        path: 'date/:id/delete',
-        component: DateDeletePopupComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Dates'
-        },
-        canActivate: [UserRouteAccessService],
-        outlet: 'popup'
-    }
-];
+    canActivate: [UserRouteAccessService]
+};
