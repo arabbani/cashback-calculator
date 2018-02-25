80cb1991a638c357ec66432f551a14166734682e
Arif Rabbani <arif.93rabbani@gmail.com>
2018-02-26 00:47:33 +0530

    Grid integrated to BankType

--- src/main/webapp/app/entities/bank-type/bank-type.component.ts
+++ src/main/webapp/app/entities/bank-type/bank-type.component.ts
@@ -1,57 +1,98 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
-import { Subscription } from 'rxjs/Subscription';
-import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { State } from '@progress/kendo-data-query';
+import * as _ from 'lodash';
+import { Observable } from 'rxjs/Observable';
 
+import { ApsstrKendoDialogService } from '../../apsstr-core-ui/apsstr-core/services';
+import { GRID_STATE } from '../../shared';
 import { BankType } from './bank-type.model';
 import { BankTypeService } from './bank-type.service';
-import { Principal } from '../../shared';
 
 @Component({
     selector: 'apsstr-bank-type',
     templateUrl: './bank-type.component.html'
 })
-export class BankTypeComponent implements OnInit, OnDestroy {
-bankTypes: BankType[];
-    currentAccount: any;
-    eventSubscriber: Subscription;
+export class BankTypeComponent implements OnInit {
 
-    constructor(
-        private bankTypeService: BankTypeService,
-        private jhiAlertService: JhiAlertService,
-        private eventManager: JhiEventManager,
-        private principal: Principal
-    ) {
+    public bankTypes: BankType[];
+    public gridState: State;
+    bankTypeFormGroup: FormGroup;
+
+    constructor(private affiliateService: BankTypeService, private formBuilder: FormBuilder,
+        private apsstrKendoDialogService: ApsstrKendoDialogService) {
+        this.createBankTypeFormGroup = this.createBankTypeFormGroup.bind(this);
     }
 
-    loadAll() {
-        this.bankTypeService.query().subscribe(
+    ngOnInit() {
+        this.gridState = GRID_STATE;
+        this.loadAllBankType();
+    }
+
+    private loadAllBankType() {
+        this.affiliateService.query().subscribe(
             (res: HttpResponse<BankType[]>) => {
                 this.bankTypes = res.body;
             },
             (res: HttpErrorResponse) => this.onError(res.message)
         );
     }
-    ngOnInit() {
-        this.loadAll();
-        this.principal.identity().then((account) => {
-            this.currentAccount = account;
+
+    public createBankTypeFormGroup(args: any): FormGroup {
+        const item = args.isNew ? new BankType() : args.dataItem;
+        this.bankTypeFormGroup = this.formBuilder.group({
+            'id': item.id,
+            'name': [item.name, Validators.required]
         });
-        this.registerChangeInBankTypes();
+        return this.bankTypeFormGroup;
     }
 
-    ngOnDestroy() {
-        this.eventManager.destroy(this.eventSubscriber);
+    public saveItem({ formGroup, isNew }): void {
+        const product = formGroup.value;
+        if (isNew) {
+            this.subscribeToSaveResponse(this.affiliateService.create(product), isNew);
+        } else {
+            this.subscribeToSaveResponse(this.affiliateService.update(product));
+        }
     }
 
-    trackId(index: number, item: BankType) {
-        return item.id;
+    public deleteItem(dataItem: any): void {
+        this.apsstrKendoDialogService.confirm().subscribe((result) => {
+            if (result['text'] === 'No') {
+                this.bankTypes.push(dataItem);
+                this.bankTypes = _.sortBy(this.bankTypes, (item) => item.id);
+            } else if (result['text'] === 'Yes') {
+                this.affiliateService.delete(dataItem.id).subscribe(
+                    (response) => {
+                        console.log('DELETED');
+                    },
+                    (error: HttpErrorResponse) => {
+                        this.loadAllBankType();
+                        this.onError(error);
+                    }
+                );
+            }
+        });
     }
-    registerChangeInBankTypes() {
-        this.eventSubscriber = this.eventManager.subscribe('bankTypeListModification', (response) => this.loadAll());
+
+    private subscribeToSaveResponse(result: Observable<HttpResponse<BankType>>, isNew?: boolean) {
+        result.subscribe((res: HttpResponse<BankType>) =>
+            this.onSaveSuccess(res.body, isNew), (res: HttpErrorResponse) => this.onSaveError());
+    }
+
+    private onSaveSuccess(result: BankType, isNew?: boolean) {
+        if (isNew && isNew === true) {
+            this.loadAllBankType();
+        }
+    }
+
+    private onSaveError() {
+        this.loadAllBankType();
     }
 
     private onError(error) {
-        this.jhiAlertService.error(error.message, null, null);
+        console.log('ERROR');
     }
+
 }
