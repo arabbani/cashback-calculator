9c18e54e2b8c7373701680f35463e595dd83ebbe
Arif Rabbani <arif.93rabbani@gmail.com>
2018-02-26 22:20:46 +0530

    Grid integrated to Category

--- src/main/webapp/app/entities/category/category.route.ts
+++ src/main/webapp/app/entities/category/category.route.ts
@@ -1,60 +1,14 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
 
 import { UserRouteAccessService } from '../../shared';
 import { CategoryComponent } from './category.component';
-import { CategoryDetailComponent } from './category-detail.component';
-import { CategoryPopupComponent } from './category-dialog.component';
-import { CategoryDeletePopupComponent } from './category-delete-dialog.component';
 
-export const categoryRoute: Routes = [
-    {
-        path: 'category',
-        component: CategoryComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Categories'
-        },
-        canActivate: [UserRouteAccessService]
-    }, {
-        path: 'category/:id',
-        component: CategoryDetailComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Categories'
-        },
-        canActivate: [UserRouteAccessService]
-    }
-];
-
-export const categoryPopupRoute: Routes = [
-    {
-        path: 'category-new',
-        component: CategoryPopupComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Categories'
-        },
-        canActivate: [UserRouteAccessService],
-        outlet: 'popup'
+export const categoryRoute: Route = {
+    path: 'category',
+    component: CategoryComponent,
+    data: {
+        authorities: ['ROLE_ADMIN'],
+        pageTitle: 'Categories'
     },
-    {
-        path: 'category/:id/edit',
-        component: CategoryPopupComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Categories'
-        },
-        canActivate: [UserRouteAccessService],
-        outlet: 'popup'
-    },
-    {
-        path: 'category/:id/delete',
-        component: CategoryDeletePopupComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Categories'
-        },
-        canActivate: [UserRouteAccessService],
-        outlet: 'popup'
-    }
-];
+    canActivate: [UserRouteAccessService]
+};
