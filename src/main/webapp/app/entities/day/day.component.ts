fd27b7ff3f7ff04e8e0a5eccc62e82fa6de5a025
Arif Rabbani <arif.93rabbani@gmail.com>
2018-02-26 11:25:13 +0530

    Grid integrated to Day

--- src/main/webapp/app/entities/day/day.component.ts
+++ src/main/webapp/app/entities/day/day.component.ts
@@ -6,52 +6,96 @@ import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
 import { Day } from './day.model';
 import { DayService } from './day.service';
 import { Principal } from '../../shared';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { State } from '@progress/kendo-data-query';
+import * as _ from 'lodash';
+import { Observable } from 'rxjs/Observable';
+
+import { ApsstrKendoDialogService } from '../../apsstr-core-ui/apsstr-core/services';
+import { GRID_STATE } from '../../shared';
 
 @Component({
     selector: 'apsstr-day',
     templateUrl: './day.component.html'
 })
-export class DayComponent implements OnInit, OnDestroy {
-days: Day[];
-    currentAccount: any;
-    eventSubscriber: Subscription;
+export class DayComponent implements OnInit {
 
-    constructor(
-        private dayService: DayService,
-        private jhiAlertService: JhiAlertService,
-        private eventManager: JhiEventManager,
-        private principal: Principal
-    ) {
+    public days: Day[];
+    public gridState: State;
+    dayFormGroup: FormGroup;
+
+    constructor(private affiliateService: DayService, private formBuilder: FormBuilder,
+        private apsstrKendoDialogService: ApsstrKendoDialogService) {
+        this.createDayFormGroup = this.createDayFormGroup.bind(this);
     }
 
-    loadAll() {
-        this.dayService.query().subscribe(
+    ngOnInit() {
+        this.gridState = GRID_STATE;
+        this.loadAllDay();
+    }
+
+    private loadAllDay() {
+        this.affiliateService.query().subscribe(
             (res: HttpResponse<Day[]>) => {
                 this.days = res.body;
             },
             (res: HttpErrorResponse) => this.onError(res.message)
         );
     }
-    ngOnInit() {
-        this.loadAll();
-        this.principal.identity().then((account) => {
-            this.currentAccount = account;
+
+    public createDayFormGroup(args: any): FormGroup {
+        const item = args.isNew ? new Day() : args.dataItem;
+        this.dayFormGroup = this.formBuilder.group({
+            'id': item.id,
+            'day': [item.day, Validators.required]
         });
-        this.registerChangeInDays();
+        return this.dayFormGroup;
     }
 
-    ngOnDestroy() {
-        this.eventManager.destroy(this.eventSubscriber);
+    public saveItem({ formGroup, isNew }): void {
+        const product = formGroup.value;
+        if (isNew) {
+            this.subscribeToSaveResponse(this.affiliateService.create(product), isNew);
+        } else {
+            this.subscribeToSaveResponse(this.affiliateService.update(product));
+        }
     }
 
-    trackId(index: number, item: Day) {
-        return item.id;
+    public deleteItem(dataItem: any): void {
+        this.apsstrKendoDialogService.confirm().subscribe((result) => {
+            if (result['text'] === 'No') {
+                this.days.push(dataItem);
+                this.days = _.sortBy(this.days, (item) => item.id);
+            } else if (result['text'] === 'Yes') {
+                this.affiliateService.delete(dataItem.id).subscribe(
+                    (response) => {
+                        console.log('DELETED');
+                    },
+                    (error: HttpErrorResponse) => {
+                        this.loadAllDay();
+                        this.onError(error);
+                    }
+                );
+            }
+        });
     }
-    registerChangeInDays() {
-        this.eventSubscriber = this.eventManager.subscribe('dayListModification', (response) => this.loadAll());
+
+    private subscribeToSaveResponse(result: Observable<HttpResponse<Day>>, isNew?: boolean) {
+        result.subscribe((res: HttpResponse<Day>) =>
+            this.onSaveSuccess(res.body, isNew), (res: HttpErrorResponse) => this.onSaveError());
+    }
+
+    private onSaveSuccess(result: Day, isNew?: boolean) {
+        if (isNew && isNew === true) {
+            this.loadAllDay();
+        }
+    }
+
+    private onSaveError() {
+        this.loadAllDay();
     }
 
     private onError(error) {
-        this.jhiAlertService.error(error.message, null, null);
+        console.log('ERROR');
     }
 }
