967bb1dd1a9195ef6907aeef1d7ca77ea204b400
Arif Rabbani <arif.93rabbani@gmail.com>
2018-02-26 22:29:18 +0530

    Grid integrated to Circle

--- src/main/webapp/app/entities/circle/circle.route.ts
+++ src/main/webapp/app/entities/circle/circle.route.ts
@@ -1,60 +1,14 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
 
 import { UserRouteAccessService } from '../../shared';
 import { CircleComponent } from './circle.component';
-import { CircleDetailComponent } from './circle-detail.component';
-import { CirclePopupComponent } from './circle-dialog.component';
-import { CircleDeletePopupComponent } from './circle-delete-dialog.component';
 
-export const circleRoute: Routes = [
-    {
-        path: 'circle',
-        component: CircleComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Circles'
-        },
-        canActivate: [UserRouteAccessService]
-    }, {
-        path: 'circle/:id',
-        component: CircleDetailComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Circles'
-        },
-        canActivate: [UserRouteAccessService]
-    }
-];
-
-export const circlePopupRoute: Routes = [
-    {
-        path: 'circle-new',
-        component: CirclePopupComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Circles'
-        },
-        canActivate: [UserRouteAccessService],
-        outlet: 'popup'
+export const circleRoute: Route = {
+    path: 'circle',
+    component: CircleComponent,
+    data: {
+        authorities: ['ROLE_ADMIN'],
+        pageTitle: 'Circles'
     },
-    {
-        path: 'circle/:id/edit',
-        component: CirclePopupComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Circles'
-        },
-        canActivate: [UserRouteAccessService],
-        outlet: 'popup'
-    },
-    {
-        path: 'circle/:id/delete',
-        component: CircleDeletePopupComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Circles'
-        },
-        canActivate: [UserRouteAccessService],
-        outlet: 'popup'
-    }
-];
+    canActivate: [UserRouteAccessService]
+};
