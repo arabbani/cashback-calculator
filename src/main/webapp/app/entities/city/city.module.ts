cd066cfe3cadf33f4d205c1834e41587eea3a078
Arif Rabbani <arif.93rabbani@gmail.com>
2018-02-26 23:33:29 +0530

    Grid integrated to City

--- src/main/webapp/app/entities/city/city.module.ts
+++ src/main/webapp/app/entities/city/city.module.ts
@@ -1,48 +1,34 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { DialogModule } from '@progress/kendo-angular-dialog';
+import { DropDownListModule } from '@progress/kendo-angular-dropdowns';
+import { GridModule } from '@progress/kendo-angular-grid';
 
 import { CbclSharedModule } from '../../shared';
-import {
-    CityService,
-    CityPopupService,
-    CityComponent,
-    CityDetailComponent,
-    CityDialogComponent,
-    CityPopupComponent,
-    CityDeletePopupComponent,
-    CityDeleteDialogComponent,
-    cityRoute,
-    cityPopupRoute,
-} from './';
+import { CityComponent, cityRoute, CityService } from './';
 
 const ENTITY_STATES = [
-    ...cityRoute,
-    ...cityPopupRoute,
+    cityRoute
 ];
 
 @NgModule({
     imports: [
         CbclSharedModule,
-        RouterModule.forChild(ENTITY_STATES)
+        RouterModule.forChild(ENTITY_STATES),
+        ReactiveFormsModule,
+        GridModule,
+        DialogModule,
+        DropDownListModule
     ],
     declarations: [
-        CityComponent,
-        CityDetailComponent,
-        CityDialogComponent,
-        CityDeleteDialogComponent,
-        CityPopupComponent,
-        CityDeletePopupComponent,
+        CityComponent
     ],
     entryComponents: [
-        CityComponent,
-        CityDialogComponent,
-        CityPopupComponent,
-        CityDeleteDialogComponent,
-        CityDeletePopupComponent,
+        CityComponent
     ],
     providers: [
-        CityService,
-        CityPopupService,
+        CityService
     ],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
--- src/main/webapp/app/entities/city/city.route.ts
+++ src/main/webapp/app/entities/city/city.route.ts
@@ -1,60 +1,14 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
 
 import { UserRouteAccessService } from '../../shared';
 import { CityComponent } from './city.component';
-import { CityDetailComponent } from './city-detail.component';
-import { CityPopupComponent } from './city-dialog.component';
-import { CityDeletePopupComponent } from './city-delete-dialog.component';
 
-export const cityRoute: Routes = [
-    {
-        path: 'city',
-        component: CityComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Cities'
-        },
-        canActivate: [UserRouteAccessService]
-    }, {
-        path: 'city/:id',
-        component: CityDetailComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Cities'
-        },
-        canActivate: [UserRouteAccessService]
-    }
-];
-
-export const cityPopupRoute: Routes = [
-    {
-        path: 'city-new',
-        component: CityPopupComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Cities'
-        },
-        canActivate: [UserRouteAccessService],
-        outlet: 'popup'
+export const cityRoute: Route = {
+    path: 'city',
+    component: CityComponent,
+    data: {
+        authorities: ['ROLE_ADMIN'],
+        pageTitle: 'Cities'
     },
-    {
-        path: 'city/:id/edit',
-        component: CityPopupComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Cities'
-        },
-        canActivate: [UserRouteAccessService],
-        outlet: 'popup'
-    },
-    {
-        path: 'city/:id/delete',
-        component: CityDeletePopupComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Cities'
-        },
-        canActivate: [UserRouteAccessService],
-        outlet: 'popup'
-    }
-];
+    canActivate: [UserRouteAccessService]
+};
