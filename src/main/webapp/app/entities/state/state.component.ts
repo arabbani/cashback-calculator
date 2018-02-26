a58a077abb18d1e6a59231421c8d2dc7e3724eae
Arif Rabbani <arif.93rabbani@gmail.com>
2018-02-27 00:00:26 +0530

    Grid integrated to State

--- src/main/webapp/app/entities/state/state.component.ts
+++ src/main/webapp/app/entities/state/state.component.ts
@@ -1,30 +1,50 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
-import { Subscription } from 'rxjs/Subscription';
-import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { State as GridState } from '@progress/kendo-data-query';
+import * as _ from 'lodash';
+import { Observable } from 'rxjs/Observable';
 
+import { ApsstrKendoDialogService } from '../../apsstr-core-ui/apsstr-core/services';
+import { GRID_STATE } from '../../shared';
+import { Country, CountryService } from '../country';
 import { State } from './state.model';
 import { StateService } from './state.service';
-import { Principal } from '../../shared';
 
 @Component({
     selector: 'apsstr-state',
     templateUrl: './state.component.html'
 })
-export class StateComponent implements OnInit, OnDestroy {
-states: State[];
-    currentAccount: any;
-    eventSubscriber: Subscription;
+export class StateComponent implements OnInit {
 
-    constructor(
-        private stateService: StateService,
-        private jhiAlertService: JhiAlertService,
-        private eventManager: JhiEventManager,
-        private principal: Principal
-    ) {
+    public states: State[];
+    public gridState: GridState;
+    stateFormGroup: FormGroup;
+
+    countries: Country[];
+    defaultCountry = {id: null, name: 'Select Country'};
+
+    constructor(private stateService: StateService, private formBuilder: FormBuilder,
+        private apsstrKendoDialogService: ApsstrKendoDialogService, private countryService: CountryService) {
+        this.createStateFormGroup = this.createStateFormGroup.bind(this);
     }
 
-    loadAll() {
+    ngOnInit() {
+        this.gridState = GRID_STATE;
+        this.loadAllCountry();
+        this.loadAllState();
+    }
+
+    private loadAllCountry() {
+        this.countryService.query().subscribe(
+            (res: HttpResponse<Country[]>) => {
+                this.countries = res.body;
+            },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
+    }
+
+    private loadAllState() {
         this.stateService.query().subscribe(
             (res: HttpResponse<State[]>) => {
                 this.states = res.body;
@@ -32,26 +52,61 @@ states: State[];
             (res: HttpErrorResponse) => this.onError(res.message)
         );
     }
-    ngOnInit() {
-        this.loadAll();
-        this.principal.identity().then((account) => {
-            this.currentAccount = account;
+
+    public createStateFormGroup(args: any): FormGroup {
+        const item = args.isNew ? new State() : args.dataItem;
+        this.stateFormGroup = this.formBuilder.group({
+            'id': item.id,
+            'name': [item.name, Validators.required],
+            'country': [item.country, Validators.required],
         });
-        this.registerChangeInStates();
+        return this.stateFormGroup;
     }
 
-    ngOnDestroy() {
-        this.eventManager.destroy(this.eventSubscriber);
+    public saveItem({ formGroup, isNew }): void {
+        const product = formGroup.value;
+        if (isNew) {
+            this.subscribeToSaveResponse(this.stateService.create(product), isNew);
+        } else {
+            this.subscribeToSaveResponse(this.stateService.update(product));
+        }
     }
 
-    trackId(index: number, item: State) {
-        return item.id;
+    public deleteItem(dataItem: any): void {
+        this.apsstrKendoDialogService.confirm().subscribe((result) => {
+            if (result['text'] === 'No') {
+                this.states.push(dataItem);
+                this.states = _.sortBy(this.states, (item) => item.id);
+            } else if (result['text'] === 'Yes') {
+                this.stateService.delete(dataItem.id).subscribe(
+                    (response) => {
+                        console.log('DELETED');
+                    },
+                    (error: HttpErrorResponse) => {
+                        this.loadAllState();
+                        this.onError(error);
+                    }
+                );
+            }
+        });
     }
-    registerChangeInStates() {
-        this.eventSubscriber = this.eventManager.subscribe('stateListModification', (response) => this.loadAll());
+
+    private subscribeToSaveResponse(result: Observable<HttpResponse<State>>, isNew?: boolean) {
+        result.subscribe((res: HttpResponse<State>) =>
+            this.onSaveSuccess(res.body, isNew), (res: HttpErrorResponse) => this.onSaveError());
+    }
+
+    private onSaveSuccess(result: State, isNew?: boolean) {
+        if (isNew && isNew === true) {
+            this.loadAllState();
+        }
+    }
+
+    private onSaveError() {
+        this.loadAllState();
     }
 
     private onError(error) {
-        this.jhiAlertService.error(error.message, null, null);
+        console.log('ERROR');
     }
 }
--- src/main/webapp/app/entities/state/state.module.ts
+++ src/main/webapp/app/entities/state/state.module.ts
@@ -1,48 +1,34 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { DialogModule } from '@progress/kendo-angular-dialog';
+import { DropDownListModule } from '@progress/kendo-angular-dropdowns';
+import { GridModule } from '@progress/kendo-angular-grid';
 
 import { CbclSharedModule } from '../../shared';
-import {
-    StateService,
-    StatePopupService,
-    StateComponent,
-    StateDetailComponent,
-    StateDialogComponent,
-    StatePopupComponent,
-    StateDeletePopupComponent,
-    StateDeleteDialogComponent,
-    stateRoute,
-    statePopupRoute,
-} from './';
+import { StateComponent, stateRoute, StateService } from './';
 
 const ENTITY_STATES = [
-    ...stateRoute,
-    ...statePopupRoute,
+    stateRoute
 ];
 
 @NgModule({
     imports: [
         CbclSharedModule,
-        RouterModule.forChild(ENTITY_STATES)
+        RouterModule.forChild(ENTITY_STATES),
+        ReactiveFormsModule,
+        GridModule,
+        DialogModule,
+        DropDownListModule
     ],
     declarations: [
-        StateComponent,
-        StateDetailComponent,
-        StateDialogComponent,
-        StateDeleteDialogComponent,
-        StatePopupComponent,
-        StateDeletePopupComponent,
+        StateComponent
     ],
     entryComponents: [
-        StateComponent,
-        StateDialogComponent,
-        StatePopupComponent,
-        StateDeleteDialogComponent,
-        StateDeletePopupComponent,
+        StateComponent
     ],
     providers: [
-        StateService,
-        StatePopupService,
+        StateService
     ],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
