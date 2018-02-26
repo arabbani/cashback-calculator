498874621bb6d479f9ff7bb21ae7f172ed285313
Arif Rabbani <arif.93rabbani@gmail.com>
2018-02-27 01:40:15 +0530

    Grid integrated to Region

--- src/main/webapp/app/entities/region/region.component.ts
+++ src/main/webapp/app/entities/region/region.component.ts
@@ -1,30 +1,38 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
-import { Subscription } from 'rxjs/Subscription';
 import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { State } from '@progress/kendo-data-query';
+import * as _ from 'lodash';
+import { Observable } from 'rxjs/Observable';
+
+import { ApsstrKendoDialogService } from '../../apsstr-core-ui/apsstr-core/services';
+import { GRID_STATE } from '../../shared';
 
 import { Region } from './region.model';
 import { RegionService } from './region.service';
-import { Principal } from '../../shared';
 
 @Component({
     selector: 'apsstr-region',
     templateUrl: './region.component.html'
 })
-export class RegionComponent implements OnInit, OnDestroy {
-regions: Region[];
-    currentAccount: any;
-    eventSubscriber: Subscription;
+export class RegionComponent implements OnInit {
 
-    constructor(
-        private regionService: RegionService,
-        private jhiAlertService: JhiAlertService,
-        private eventManager: JhiEventManager,
-        private principal: Principal
-    ) {
+    public regions: Region[];
+    public gridState: State;
+    returnModeFormGroup: FormGroup;
+
+    constructor(private regionService: RegionService, private formBuilder: FormBuilder,
+        private apsstrKendoDialogService: ApsstrKendoDialogService) {
+        this.createRegionFormGroup = this.createRegionFormGroup.bind(this);
     }
 
-    loadAll() {
+    ngOnInit() {
+        this.gridState = GRID_STATE;
+        this.loadAllRegion();
+    }
+
+    private loadAllRegion() {
         this.regionService.query().subscribe(
             (res: HttpResponse<Region[]>) => {
                 this.regions = res.body;
@@ -32,26 +40,60 @@ regions: Region[];
             (res: HttpErrorResponse) => this.onError(res.message)
         );
     }
-    ngOnInit() {
-        this.loadAll();
-        this.principal.identity().then((account) => {
-            this.currentAccount = account;
+
+    public createRegionFormGroup(args: any): FormGroup {
+        const item = args.isNew ? new Region() : args.dataItem;
+        this.returnModeFormGroup = this.formBuilder.group({
+            'id': item.id,
+            'name': [item.name, Validators.required]
         });
-        this.registerChangeInRegions();
+        return this.returnModeFormGroup;
     }
 
-    ngOnDestroy() {
-        this.eventManager.destroy(this.eventSubscriber);
+    public saveItem({ formGroup, isNew }): void {
+        const product = formGroup.value;
+        if (isNew) {
+            this.subscribeToSaveResponse(this.regionService.create(product), isNew);
+        } else {
+            this.subscribeToSaveResponse(this.regionService.update(product));
+        }
     }
 
-    trackId(index: number, item: Region) {
-        return item.id;
+    public deleteItem(dataItem: any): void {
+        this.apsstrKendoDialogService.confirm().subscribe((result) => {
+            if (result['text'] === 'No') {
+                this.regions.push(dataItem);
+                this.regions = _.sortBy(this.regions, (item) => item.id);
+            } else if (result['text'] === 'Yes') {
+                this.regionService.delete(dataItem.id).subscribe(
+                    (response) => {
+                        console.log('DELETED');
+                    },
+                    (error: HttpErrorResponse) => {
+                        this.loadAllRegion();
+                        this.onError(error);
+                    }
+                );
+            }
+        });
     }
-    registerChangeInRegions() {
-        this.eventSubscriber = this.eventManager.subscribe('regionListModification', (response) => this.loadAll());
+
+    private subscribeToSaveResponse(result: Observable<HttpResponse<Region>>, isNew?: boolean) {
+        result.subscribe((res: HttpResponse<Region>) =>
+            this.onSaveSuccess(res.body, isNew), (res: HttpErrorResponse) => this.onSaveError());
+    }
+
+    private onSaveSuccess(result: Region, isNew?: boolean) {
+        if (isNew && isNew === true) {
+            this.loadAllRegion();
+        }
+    }
+
+    private onSaveError() {
+        this.loadAllRegion();
     }
 
     private onError(error) {
-        this.jhiAlertService.error(error.message, null, null);
+        console.log('ERROR');
     }
 }
