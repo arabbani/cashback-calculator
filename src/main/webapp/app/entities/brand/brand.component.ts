c89400a58e395f6e4341e5d92f0f52d29d360a9c
Arif Rabbani <arif.93rabbani@gmail.com>
2018-02-27 23:56:11 +0530

    Grid integrated to Brand

--- src/main/webapp/app/entities/brand/brand.component.ts
+++ src/main/webapp/app/entities/brand/brand.component.ts
@@ -1,30 +1,50 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
-import { Subscription } from 'rxjs/Subscription';
-import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { State } from '@progress/kendo-data-query';
+import * as _ from 'lodash';
+import { Observable } from 'rxjs/Observable';
 
+import { ApsstrKendoDialogService } from '../../apsstr-core-ui/apsstr-core/services';
+import { GRID_STATE } from '../../shared';
+import { SubCategory, SubCategoryService } from '../sub-category';
 import { Brand } from './brand.model';
 import { BrandService } from './brand.service';
-import { Principal } from '../../shared';
 
 @Component({
     selector: 'apsstr-brand',
     templateUrl: './brand.component.html'
 })
-export class BrandComponent implements OnInit, OnDestroy {
-brands: Brand[];
-    currentAccount: any;
-    eventSubscriber: Subscription;
+export class BrandComponent implements OnInit {
 
-    constructor(
-        private brandService: BrandService,
-        private jhiAlertService: JhiAlertService,
-        private eventManager: JhiEventManager,
-        private principal: Principal
-    ) {
+    public brands: Brand[];
+    public gridState: State;
+    brandFormGroup: FormGroup;
+
+    subCategories: SubCategory[];
+    defaultSubCategory = 'Select Sub Categories';
+
+    constructor(private brandService: BrandService, private formBuilder: FormBuilder,
+        private apsstrKendoDialogService: ApsstrKendoDialogService, private subCategoryService: SubCategoryService) {
+        this.createBrandFormGroup = this.createBrandFormGroup.bind(this);
     }
 
-    loadAll() {
+    ngOnInit() {
+        this.gridState = GRID_STATE;
+        this.loadAllSubCategories();
+        this.loadAllBrand();
+    }
+
+    private loadAllSubCategories() {
+        this.subCategoryService.query().subscribe(
+            (res: HttpResponse<SubCategory[]>) => {
+                this.subCategories = res.body;
+            },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
+    }
+
+    private loadAllBrand() {
         this.brandService.query().subscribe(
             (res: HttpResponse<Brand[]>) => {
                 this.brands = res.body;
@@ -32,26 +52,75 @@ brands: Brand[];
             (res: HttpErrorResponse) => this.onError(res.message)
         );
     }
-    ngOnInit() {
-        this.loadAll();
-        this.principal.identity().then((account) => {
-            this.currentAccount = account;
+
+    public createBrandFormGroup(args: any): FormGroup {
+        const item = args.isNew ? new Brand() : args.dataItem;
+        this.brandFormGroup = this.formBuilder.group({
+            'id': item.id,
+            'name': [item.name, Validators.required],
+            'subCategories': [item.subCategories]
         });
-        this.registerChangeInBrands();
+        return this.brandFormGroup;
     }
 
-    ngOnDestroy() {
-        this.eventManager.destroy(this.eventSubscriber);
+    public saveItem({ formGroup, isNew }): void {
+        const product = formGroup.value;
+        if (isNew) {
+            this.subscribeToSaveResponse(this.brandService.create(product), isNew);
+        } else {
+            this.subscribeToSaveResponse(this.brandService.update(product));
+        }
     }
 
-    trackId(index: number, item: Brand) {
-        return item.id;
+    public deleteItem(dataItem: any): void {
+        this.apsstrKendoDialogService.confirm().subscribe((result) => {
+            if (result['text'] === 'No') {
+                this.brands.push(dataItem);
+                this.brands = _.sortBy(this.brands, (item) => item.id);
+            } else if (result['text'] === 'Yes') {
+                this.brandService.delete(dataItem.id).subscribe(
+                    (response) => {
+                        console.log('DELETED');
+                    },
+                    (error: HttpErrorResponse) => {
+                        this.loadAllBrand();
+                        this.onError(error);
+                    }
+                );
+            }
+        });
     }
-    registerChangeInBrands() {
-        this.eventSubscriber = this.eventManager.subscribe('brandListModification', (response) => this.loadAll());
+
+    private subscribeToSaveResponse(result: Observable<HttpResponse<Brand>>, isNew?: boolean) {
+        result.subscribe((res: HttpResponse<Brand>) =>
+            this.onSaveSuccess(res.body, isNew), (res: HttpErrorResponse) => this.onSaveError());
+    }
+
+    private onSaveSuccess(result: Brand, isNew?: boolean) {
+        if (isNew && isNew === true) {
+            this.loadAllBrand();
+        }
+    }
+
+    private onSaveError() {
+        this.loadAllBrand();
     }
 
     private onError(error) {
-        this.jhiAlertService.error(error.message, null, null);
+        console.log('ERROR');
+    }
+
+    public constructSubCategoryNames(subCategories: SubCategory[]): string {
+        let subCategoriesName = '';
+        if (subCategories) {
+            const length = subCategories.length;
+            _.forEach(subCategories, function (value, index) {
+                subCategoriesName += value.name;
+                if (index < length - 1) {
+                    subCategoriesName += ', ';
+                }
+            });
+        }
+        return subCategoriesName;
     }
 }
--- src/main/webapp/app/entities/brand/brand.route.ts
+++ src/main/webapp/app/entities/brand/brand.route.ts
@@ -1,60 +1,14 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
 
 import { UserRouteAccessService } from '../../shared';
 import { BrandComponent } from './brand.component';
-import { BrandDetailComponent } from './brand-detail.component';
-import { BrandPopupComponent } from './brand-dialog.component';
-import { BrandDeletePopupComponent } from './brand-delete-dialog.component';
 
-export const brandRoute: Routes = [
-    {
-        path: 'brand',
-        component: BrandComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Brands'
-        },
-        canActivate: [UserRouteAccessService]
-    }, {
-        path: 'brand/:id',
-        component: BrandDetailComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Brands'
-        },
-        canActivate: [UserRouteAccessService]
-    }
-];
-
-export const brandPopupRoute: Routes = [
-    {
-        path: 'brand-new',
-        component: BrandPopupComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Brands'
-        },
-        canActivate: [UserRouteAccessService],
-        outlet: 'popup'
+export const brandRoute: Route = {
+    path: 'brand',
+    component: BrandComponent,
+    data: {
+        authorities: ['ROLE_ADMIN'],
+        pageTitle: 'Brands'
     },
-    {
-        path: 'brand/:id/edit',
-        component: BrandPopupComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Brands'
-        },
-        canActivate: [UserRouteAccessService],
-        outlet: 'popup'
-    },
-    {
-        path: 'brand/:id/delete',
-        component: BrandDeletePopupComponent,
-        data: {
-            authorities: ['ROLE_ADMIN'],
-            pageTitle: 'Brands'
-        },
-        canActivate: [UserRouteAccessService],
-        outlet: 'popup'
-    }
-];
+    canActivate: [UserRouteAccessService]
+};
