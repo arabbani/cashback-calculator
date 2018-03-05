c308384d4899ecb6bc25a83b3d879276692d154f
Arif Rabbani <arif.93rabbani@gmail.com>
2018-03-05 23:40:24 +0530

    Fix

--- src/main/webapp/app/entities/offer/create-offer/create-offer.component.ts
+++ src/main/webapp/app/entities/offer/create-offer/create-offer.component.ts
@@ -15,6 +15,9 @@ import { State, StateService } from '../../state';
 import { OperatingSystem, OperatingSystemService } from '../../operating-system';
 import { Affiliate, AffiliateService } from '../../affiliate';
 import { Merchant, MerchantService } from '../../merchant';
+import { Category, CategoryService } from '../../category';
+import { SubCategory, SubCategoryService } from '../../sub-category';
+import { ServiceProvider, ServiceProviderService } from '../../service-provider';
 
 @Component({
   selector: 'apsstr-create-offer',
@@ -36,7 +39,12 @@ export class CreateOfferComponent implements OnInit {
   filteredCities: City[];
   operatingSystems: OperatingSystem[];
   affiliates: Affiliate[];
-  merchants: Merchant[]; l
+  merchants: Merchant[];
+  categories: Category[];
+  subCategories: SubCategory[];
+  filteredSubCategories: SubCategory[];
+  serviceProviders: ServiceProvider[];
+  filteredServiceProviders: ServiceProvider[];
 
   isCoupon: boolean;
   defaultOfferType;
@@ -49,12 +57,17 @@ export class CreateOfferComponent implements OnInit {
   defaultOperatingSystem;
   defaultAffiliate;
   defaultMerchant;
+  defaultCategory;
+  defaultSubCategory;
+  defaultServiceProvider;
 
   enabledTabs: Array<boolean>;
+  offerCategory: Category;
 
   constructor(private offerService: OfferService, private offerTypeService: OfferTypeService, private offerPolicyService: OfferPolicyService, private dateService: DateService,
     private dayService: DayService, private countryService: CountryService, private stateService: StateService, private cityService: CityService,
-    private operatingSystemService: OperatingSystemService, private affiliateService: AffiliateService, private merchantService: MerchantService) { }
+    private operatingSystemService: OperatingSystemService, private affiliateService: AffiliateService, private merchantService: MerchantService,
+    private categoryService: CategoryService, private subCategoryService: SubCategoryService, private serviceProviderService: ServiceProviderService) { }
 
   ngOnInit() {
     this.initialize();
@@ -68,11 +81,14 @@ export class CreateOfferComponent implements OnInit {
     this.loadOperatingSystems();
     this.loadAffiliates();
     this.loadMerchants();
+    this.loadCategories();
+    this.loadSubCategories();
+    this.loadServiceProviders();
     this.offer = new Offer();
   }
 
   initialize(): void {
-    this.enabledTabs = _.times(2, _.stubFalse);
+    this.enabledTabs = _.times(5, _.stubFalse);
     this.enabledTabs[0] = true;
     this.isCoupon = false;
     this.defaultOfferType = { id: null, name: 'Select Type' };
@@ -85,6 +101,9 @@ export class CreateOfferComponent implements OnInit {
     this.defaultOperatingSystem = { id: null, name: 'Select OS' };
     this.defaultAffiliate = { id: null, name: 'Select Affiliate' };
     this.defaultMerchant = { id: null, name: 'Select Merchant' };
+    this.defaultCategory = { id: null, name: 'Select Category' };
+    this.defaultSubCategory = { id: null, name: 'Select Sub-Category' };
+    this.defaultServiceProvider = { id: null, name: 'Select Service Provider' };
   }
 
   loadOfferTypes(): void {
@@ -179,6 +198,35 @@ export class CreateOfferComponent implements OnInit {
     );
   }
 
+  loadCategories(): void {
+    this.categoryService.query().subscribe(
+      (res: HttpResponse<Category[]>) => {
+        this.categories = res.body;
+      },
+      (res: HttpErrorResponse) => this.onError(res.message)
+    );
+  }
+
+  loadSubCategories(): void {
+    this.subCategoryService.query().subscribe(
+      (res: HttpResponse<SubCategory[]>) => {
+        this.subCategories = res.body;
+        this.filteredSubCategories = this.subCategories;
+      },
+      (res: HttpErrorResponse) => this.onError(res.message)
+    );
+  }
+
+  loadServiceProviders(): void {
+    this.serviceProviderService.query().subscribe(
+      (res: HttpResponse<ServiceProvider[]>) => {
+        this.serviceProviders = res.body;
+        this.filteredServiceProviders = this.serviceProviders;
+      },
+      (res: HttpErrorResponse) => this.onError(res.message)
+    );
+  }
+
   goToNextTab(tabNumber: number): void {
     this.enabledTabs[tabNumber] = true;
     this.createOfferTabs.tabs[tabNumber].active = true;
@@ -201,14 +249,31 @@ export class CreateOfferComponent implements OnInit {
   }
 
   onCountryChange(country: Country): void {
-    this.filteredStates = _.filter(this.states, (state) => state.country.name === country.name);
+    this.filteredStates = _.filter(this.states, (state) => state.country.id === country.id);
     this.offer.states = undefined;
     this.offer.cities = undefined;
   }
 
   onStateChange(state: State): void {
-    this.filteredCities = _.filter(this.cities, (city) => city.state.name === state.name);
+    this.filteredCities = _.filter(this.cities, (city) => city.state.id === state.id);
     this.offer.cities = undefined;
+  }
+
+  onCategoryChange(category: Category): void {
+    this.offerCategory = category;
+    this.filteredSubCategories = _.filter(this.subCategories, (subCategory) => subCategory.category.id === this.offerCategory.id);
+  }
+
+  onSubCategoryChange(subCategory: SubCategory): void {
+    let found;
+    this.filteredServiceProviders = _.filter(this.serviceProviders, (serviceProvider) => {
+      found = _.find(serviceProvider.subCategories, (sCategory) => subCategory.id === sCategory.id);
+      if (found) {
+        return true;
+      } else {
+        return false;
+      }
+    });
   }
 
   private onError(error) {
