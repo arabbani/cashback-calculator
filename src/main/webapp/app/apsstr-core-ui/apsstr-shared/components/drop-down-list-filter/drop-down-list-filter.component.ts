739db74fa61b319050f3bbf2f4eca6b83534a980
Arif Rabbani <arif.93rabbani@gmail.com>
2018-03-11 16:12:40 +0530

    Improvement

--- src/main/webapp/app/apsstr-core-ui/apsstr-shared/components/drop-down-list-filter/drop-down-list-filter.component.ts
+++ src/main/webapp/app/apsstr-core-ui/apsstr-shared/components/drop-down-list-filter/drop-down-list-filter.component.ts
@@ -1,13 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { BaseFilterCellComponent, FilterService } from '@progress/kendo-angular-grid';
 import { CompositeFilterDescriptor } from '@progress/kendo-data-query';
+
+import { FilterEntitiesService } from '../../../apsstr-core/services';
 
 @Component({
   selector: 'apsstr-drop-down-list-filter',
   templateUrl: './drop-down-list-filter.component.html',
   styles: []
 })
-export class DropDownListFilterComponent extends BaseFilterCellComponent {
+export class DropDownListFilterComponent extends BaseFilterCellComponent implements OnChanges {
 
   public get selectedValue(): any {
     const filter = this.filterByField(this.valueField);
@@ -15,12 +17,25 @@ export class DropDownListFilterComponent extends BaseFilterCellComponent {
   }
 
   @Input() public filter: CompositeFilterDescriptor;
-  @Input() public data: any[];
+  @Input() private data: any[];
   @Input() public textField: string;
   @Input() public valueField: string;
+  @Input() public filterable?: boolean;
+  public filteredData: any[];
 
-  constructor(filterService: FilterService) {
+  constructor(filterService: FilterService, private filterEntitiesService: FilterEntitiesService) {
     super(filterService);
+  }
+
+  public get defaultItem(): any {
+    return {
+      [this.textField]: 'Select item...',
+      [this.valueField]: null
+    };
+  }
+
+  ngOnChanges() {
+    this.filteredData = this.data;
   }
 
   public onChange(value: any): void {
@@ -33,6 +48,10 @@ export class DropDownListFilterComponent extends BaseFilterCellComponent {
           value
         })
     ); // update the root filter
+  }
+
+  filterData(value) {
+    this.filteredData = this.filterEntitiesService.byStringAttribute(this.data, this.textField, value);
   }
 
 }
